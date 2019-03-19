087edb2a2f34399c9743feb7d98b67f24d34143e
Aakash Goel <goelaakash79@gmail.com>
2019-03-19 16:53:47 +0530

    folder structure added

--- index.js
+++ index.js
@@ -1,68 +1,67 @@
 const express = require("express");
 const mongoose = require('mongoose');
 const path = require("path");
-const bodyparser = require("body-parser");
+const bodyParser = require("body-parser");
+const cors = require('cors');
+
 const app = express();
-const dburl = "mongodb://dsckiet:dsc123@ds153495.mlab.com:53495/dsckiet-demo-db";
-var qw;
-mongoose.Promise = global.Promise;
+
+require('dotenv').config();
+require('./config/dbconnection');
+
+app.use(cors());
+app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyparser.urlencoded({extended:true}));
-const data = require("./models/datafile.js")
-mongoose.connect(dburl,(err) => {
-    if(err)
-        console.log(err);
-    else
-        console.log("Database connected");
+app.use(bodyParser.urlencoded({extended: true}));
+
+const User = require("./models/User");
+
+app.get("/login", (req, res) => {
+    res.render("index1");//res.send agar kuch value bhejni h aur html file ke liye res.render
 });
 
-app.get("/login",(req, res) =>{
-    res.render("index1.ejs");
+app.get("/register", (req, res) => {
+    res.render("index");//res.send agar kuch value bhejni h aur html file ke liye res.render
 });
 
-app.get("/",(req, res) => {
+app.get("/login", (req, res) => {
+    res.render("index1");
+});
+
+app.get("/", (req, res) => {
     //req from client side
     //jobhi response client ko bhejenge
-    data.find({}, (err, datas)=>{
-        if(err)
+    User.find({}, (err, datas) => {
+        if (err)
             console.log(err);
         console.log(datas);
         res.send(datas);
     });
 });
 
-app.post("/register", (req, res)=>{
+app.post("/register", (req, res) => {
     console.log(req.body);
-    data.findOne({name: req.body.name}, (err, entry)=>{
-        if(err) console.log(err);
-        else if(entry)
-            {
-                res.render("index1.ejs")
-            }
-        else
-            {
-            data.create({name:req.body.name, email:req.body.email, password: req.body.password1},(err, done)=>{
-            if(err)
-            {
-                res.render("error.ejs",{message: "error"});
-            }
-            else
-            {
-                res.render("index1.ejs");
-            }
-    });
-            }
+    User.findOne({name: req.body.name}, (err, entry) => {
+        if (err) console.log(err);
+        else if (entry) {
+            res.render("index1")
+        } else {
+            User.create({name: req.body.name, email: req.body.email, password: req.body.password1}, (err, done) => {
+                if (err) {
+                    res.render("error", {message: "error"});
+                } else {
+                    res.render("index1");
+                }
+            });
+        }
     })
 
 });
 
-
-app.get("/login",(req, res) => {
-    res.render("index1.ejs");//res.send agar kuch value bhejni h aur html file ke liye res.render
+app.listen(process.env.PORT, (err) => {
+    if (err) {
+        console.log("Error in running server");
+        return;
+    }
+    console.log(`Server is up and running on http://localhost:${process.env.PORT}`);
 });
-
-app.get("/register",(req, res) => {
-    res.render("index.ejs");//res.send agar kuch value bhejni h aur html file ke liye res.render
-});
-
-app.listen(4000, ()=> console.log("server started"));
